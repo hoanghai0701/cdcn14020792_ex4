d9e2d94231ff941d1a2f10b14e0b1c1f702a4b52
hainh.ownego <developers@ownego.com>
2017-03-14 22:45:38 +0700

    Update: Restful service

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,7 @@ router.get('/', (req, res, next) => {
   let db = req.db;
   let collection = db.get('user');
   collection.find({}, {}, (e, docs) => {
-    res.render('user-list', {
-      'userList' : docs
-    })
+    res.json(docs);
   });
 });
 
@@ -18,18 +16,19 @@ router.get('/create', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   let db = req.db;
-  console.log(req);
-
   let collection = db.get('user');
-  collection.insert({
-    username: req.body.username,
-    email: req.body.email
-  }, (err, doc) => {
-    if (err) {
-      res.send("Creating user failed.");
-    } else {
-      res.redirect('/users');
-    }
+  collection.insert(req.body, (err, doc) => {
+    res.send((err === null) ? {msg: ""} : {msg: err})
   });
 });
+
+router.delete('/:id', (req, res, next) => {
+  let db = req.db;
+  let collection = db.get('user');
+  let userId = req.params.id;
+  collection.remove({'_id': userId}, err => {
+    res.send((err === null) ? {msg: ""} : {msg: err})
+  });
+});
+
 module.exports = router;
